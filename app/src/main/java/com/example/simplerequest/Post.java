4144f9c84c62228602bd787df2f854e61ecd2b2b
xPromate <jpcm_1998@hotmail.com>
2020-08-28 15:06:04 +0100

    simple request project

---
+++ app/src/main/java/com/example/simplerequest/Post.java
@@ -0,0 +1,24 @@
+package com.example.simplerequest;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+    private int id;
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+    public int getUserId() {
+        return userId;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getText() {
+        return text;
+    }
+}
